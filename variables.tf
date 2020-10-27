07b83b985ed2566b30b2a9cb2a2f303afd663ef7
Benjamin Sherwood <benjamin.sherwood@dmwgroup.co.uk>
2020-10-27 11:43:46 +0000

    DW-5194 - Deploy Tarball Ingester (#1)

--- variables.tf
+++ variables.tf
@@ -13,3 +13,45 @@ variable "region" {
   type    = string
   default = "eu-west-2"
 }
+
+variable "al2_hardened_ami_id" {
+  description = "The AMI ID of the latest/pinned Hardened AMI AL2 Image"
+  type        = string
+}
+
+variable "tarball_ingester_ec2_size" {
+  default = {
+    development = "t3.medium"
+    qa          = "t3.medium"
+    integration = "t3.medium"
+    preprod     = "t3.medium"
+    production  = "t3.medium"
+  }
+}
+
+variable "tarball_ingester_release" {
+  description = "Release number for the Tarball ingester release"
+  type        = string
+}
+
+variable "tarball_ingester_asg_desired" {
+  description = "Desired tarball_ingester equality ASG size"
+  default = {
+    development = 1
+    qa          = 1
+    integration = 1
+    preprod     = 1
+    production  = 1
+  }
+}
+
+variable "tarball_ingester_asg_max" {
+  description = "Max tarball_ingester ASG size."
+  default = {
+    development = 1
+    qa          = 1
+    integration = 1
+    preprod     = 1
+    production  = 1
+  }
+}

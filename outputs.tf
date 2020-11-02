561b070bbb9b106b70f4c89de944044efb017994
Benjamin Sherwood <benjamin.sherwood@dmwgroup.co.uk>
2020-11-02 11:52:03 +0000

    DW-5193 - Add output for Ingester ASG
    Adding output for Ingester ASG

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "tarball_ingester_sg" {
   value = aws_security_group.tarball_ingester
 }
+
+output "ingester_asg" {
+  value = {
+    name = aws_autoscaling_group.tarball_ingester.name
+  }
+}

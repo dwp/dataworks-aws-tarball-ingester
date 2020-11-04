eb5ff8493be5ed5b6126a37cca03e5f03bf77839
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-11-04 13:23:15 +0000

    DW-5237 - Restructure
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

---
+++ tarball-ingester-files.tf
@@ -0,0 +1,71 @@
+data "local_file" "tarball_ingester_logrotate_script" {
+  filename = "files/tarball_ingester.logrotate"
+}
+
+resource "aws_s3_bucket_object" "tarball_ingester_logrotate_script" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/tarball-ingester/tarball-ingester.logrotate"
+  content    = data.local_file.tarball_ingester_logrotate_script.content
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "tarball-ingester-logrotate-script"
+    },
+  )
+}
+
+data "local_file" "tarball_ingester_cloudwatch_script" {
+  filename = "files/tarball_ingester_cloudwatch.sh"
+}
+
+resource "aws_s3_bucket_object" "tarball_ingester_cloudwatch_script" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/tarball-ingester/tarball-ingester-cloudwatch.sh"
+  content    = data.local_file.tarball_ingester_cloudwatch_script.content
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "tarball-ingester-cloudwatch-script"
+    },
+  )
+}
+
+data "local_file" "tarball_ingester_minio_script" {
+  filename = "files/tarball_ingester_minio.sh"
+}
+
+resource "aws_s3_bucket_object" "tarball_ingester_minio_script" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/tarball-ingester/tarball-ingester-minio.sh"
+  content    = data.local_file.tarball_ingester_minio_script.content
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "tarball-ingester-minio-script"
+    },
+  )
+}
+
+data "local_file" "tarball_ingester_minio_service_file" {
+  filename = "files/minio.service"
+}
+
+resource "aws_s3_bucket_object" "tarball_ingester_minio_service_file" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/tarball-ingester/minio.service"
+  content    = data.local_file.tarball_ingester_minio_service_file.content
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "tarball-ingester-minio-service-file"
+    },
+  )
+}
---
+++ tarball-ingester-iam.tf
@@ -0,0 +1,211 @@
+data "aws_iam_policy_document" "tarball_ingester" {
+  statement {
+    sid    = "AllowACM"
+    effect = "Allow"
+
+    actions = [
+      "acm:*Certificate",
+    ]
+
+    resources = [aws_acm_certificate.tarball_ingester.arn]
+  }
+
+  statement {
+    sid    = "GetPublicCerts"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [data.terraform_remote_state.certificate_authority.outputs.public_cert_bucket.arn]
+  }
+
+  statement {
+    sid    = "AllowUseDefaultEbsCmk"
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+
+    resources = [data.terraform_remote_state.security-tools.outputs.ebs_cmk.arn]
+  }
+
+  statement {
+    effect = "Allow"
+    sid    = "AllowAccessToConfigBucket"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+
+
+    resources = [data.terraform_remote_state.common.outputs.config_bucket.arn]
+  }
+
+  statement {
+    effect = "Allow"
+    sid    = "AllowAccessToConfigBucketObjects"
+
+    actions = ["s3:GetObject"]
+
+    resources = ["${data.terraform_remote_state.common.outputs.config_bucket.arn}/*"]
+  }
+
+  statement {
+    sid    = "AllowKMSDecryptionOfS3ConfigBucketObj"
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+    ]
+
+    resources = [data.terraform_remote_state.common.outputs.config_bucket_cmk.arn]
+  }
+
+  statement {
+    sid       = "AllowDescribeASGToCheckLaunchTemplate"
+    effect    = "Allow"
+    actions   = ["autoscaling:DescribeAutoScalingGroups"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid       = "AllowDescribeEC2LaunchTemplatesToCheckLatestVersion"
+    effect    = "Allow"
+    actions   = ["ec2:DescribeLaunchTemplates"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "TarballingesterKMS"
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    resources = [
+      data.terraform_remote_state.ingest.outputs.input_bucket_cmk.arn
+    ]
+  }
+
+  statement {
+    sid     = "AllowAccessToArtefactBucket"
+    effect  = "Allow"
+    actions = ["s3:GetBucketLocation"]
+
+    resources = [data.terraform_remote_state.management_artefact.outputs.artefact_bucket.arn]
+  }
+
+  statement {
+    sid       = "AllowPullFromArtefactBucket"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${data.terraform_remote_state.management_artefact.outputs.artefact_bucket.arn}/*"]
+  }
+
+  statement {
+    sid    = "AllowDecryptArtefactBucket"
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+    ]
+
+    resources = [data.terraform_remote_state.management_artefact.outputs.artefact_bucket.cmk_arn]
+  }
+
+  statement {
+    sid    = "AllowTarballingesterToAccessLogGroups"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams"
+    ]
+    resources = [aws_cloudwatch_log_group.tarball_ingester_logs.arn]
+  }
+}
+
+data "aws_iam_policy_document" "tarball_ingester_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "minio_credentials_secretsmanager" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+
+    resources = [
+      aws_secretsmanager_secret.minio_credentials.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "tarball_ingester" {
+  name        = "tarball_ingester"
+  description = "Policy to allow access for Tarball ingester"
+  policy      = data.aws_iam_policy_document.tarball_ingester.json
+}
+
+resource "aws_iam_policy" "minio_credentials_secretsmanager" {
+  name        = "MiniIOSecretsManager"
+  description = "Allow reading of MinIO Access and Secret Keys"
+  policy      = data.aws_iam_policy_document.minio_credentials_secretsmanager.json
+}
+
+resource "aws_iam_role_policy_attachment" "tarball_ingester" {
+  role       = aws_iam_role.tarball_ingester.name
+  policy_arn = aws_iam_policy.tarball_ingester.arn
+}
+
+resource "aws_iam_role_policy_attachment" "tarball_ingester_cwasp" {
+  role       = aws_iam_role.tarball_ingester.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "tarball_ingester_ssm" {
+  role       = aws_iam_role.tarball_ingester.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_role_policy_attachment" "tarball_ingester_minio" {
+  role       = aws_iam_role.tarball_ingester.name
+  policy_arn = aws_iam_policy.minio_credentials_secretsmanager.arn
+}
+
+resource "aws_iam_role" "tarball_ingester" {
+  name                 = "tarball_ingester"
+  assume_role_policy   = data.aws_iam_policy_document.tarball_ingester_policy.json
+  max_session_duration = local.iam_role_max_session_timeout_seconds
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "tarball_ingester"
+    },
+  )
+}

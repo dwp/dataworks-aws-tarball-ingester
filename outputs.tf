3f85512c4814dc80a0c24e7a1c7374c56f84d327
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-11 09:27:33 +0100

    Remove unused output (#56)

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "tarball_ingester_sg" {
-  value = aws_security_group.tarball_ingester
-}
-
 output "tarball_ingester_asg" {
   value = {
     name = aws_autoscaling_group.tarball_ingester.name
